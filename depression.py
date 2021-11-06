5add30a026ba3d965217f3a554dc7316f2970595
vimalgupta24 <vimar4bsa@gmail.com>
2021-11-06 22:23:22 +0530

    final changes to sidebar

--- depression.py
+++ depression.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
 def app():
+    st.markdown("# Hope You Score Less Than You Expect,Good Luck!")
+    st.header("------------------")
+    "\n"
     st.write("Below is a list of few questions designed to help you determine if you might be experiencing depression,anxiety. The questions relate to life experiences common among people who have such mental health problems. Please read each question carefully, and indicate how often you have experienced the same or similar challenges in the past few weeks.")
     # depr = st.button("Test2")
     st.success("depressed")
--- home.py
+++ home.py
@@ -2,5 +2,4 @@ import streamlit as st
 from PIL import Image
 
 def app():
-    st.markdown("# Hope You Score Less Than You Expect,Good Luck!")
-    st.header("------------------")
+    st.markdown("# welcome ")
